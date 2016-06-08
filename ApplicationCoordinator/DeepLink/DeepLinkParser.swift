b4c90e57f48932d41501280c52277a6566a42567
Andrey Panov <apanov@avito.ru>
2016-06-08 10:15:28 +0300

    files add

---
+++ ApplicationCoordinator/DeepLink/DeepLinkParser.swift
@@ -0,0 +1,9 @@
+//
+//  DeepLinkParser.swift
+//  ApplicationCoordinator
+//
+//  Created by Панов Андрей on 08.06.16.
+//  Copyright © 2016 Andrey Panov. All rights reserved.
+//
+
+import Foundation
